9a97ac5f216fe1aaf5a9e5d37b6a9a40f7f2c78a
Shacent <40484440+Shacent@users.noreply.github.com>
2025-08-17 15:32:20 +0700

    Update app.blade.php

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     @include('partials.head')
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 
 <body class="min-h-screen bg-[#FAF7F3] grain-texture text-[#4A453E] antialiased">
